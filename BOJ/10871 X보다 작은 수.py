d8f8c486233a8fc8a5ce98598f2cb752f0cf9cca
jyj2187 <jyjgom@gmail.com>
2022-06-27 22:03:09 +0900

    알고리즘 문제 분리

--- BOJ/10871 X보다 작은 수.py
+++
@@ -1,9 +0,0 @@
-n, x = map(int, input().split())
-data = list(map(int, input().split()))
-
-for i in range(n):
-    if data[i] < x:
-        print(data[i], end=" ")
-
-# my_list = [i for i in A if i<X ]
-# print(" ".join(map(str, my_list)))
--- 모두의 알고리즘/1. 알고리즘 기초/p01 1부터 n까지 합 구하기/p01-2-sum.py
+++
@@ -1,8 +0,0 @@
-# 1부터 n까지의 합을 구하는 알고리즘 2
-
-def sum_n(n):
-    return n * (n + 1) // 2  # 슬래시 2개는 정수의 나눗셈
-
-
-print(sum_n(10))
-print(sum_n(100))
